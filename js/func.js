250b31b3ed57f03ba7606f6abfcbee4edb7f8e2c
Lars von der Burg <vonderburg.lars@gmail.com>
2016-07-09 02:15:53 +0200

    Set up basic JS structure

--- js/func.js
+++ js/func.js
@@ -1,3 +1,14 @@
 /**
  * Created by larsv on 27/06/2016.
  */
+// TODO{Lars} Implement pubsub
+
+// TODO{Lars} Build updateWorkTime module
+
+// TODO{Lars} Build updateRestTime module
+
+// TODO{Lars} Build runWork module
+
+// TODO{Lars} Build runRest module
+
+// TODO{Lars} Handle pauses through events
